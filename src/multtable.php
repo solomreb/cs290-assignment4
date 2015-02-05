8a9701e43770557e7201145b4976c6319f2526c3
solomreb <solomreb@onid.oregonstate.edu>
2015-02-05 14:59:19 -0800

    validates input including non-integers

--- src/multtable.php
+++ src/multtable.php
@@ -3,6 +3,8 @@
 <body>
 
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors',1);
 
 $params = array('min-multiplicand' => $_GET["min-multiplicand"], 'max-multiplicand' =>$_GET["max-multiplicand"], 'min-multiplier' => $_GET["min-multiplier"], 'max-multiplier' => $_GET["max-multiplier"]);
 
@@ -11,16 +13,17 @@ foreach ($params as $k => $v){
 	if ($v === ''){
 		echo "Missing parameter " .$k. ".<br>";
 	}
-	elseif (!is_int($v)){
+	elseif (!filter_var($v, FILTER_VALIDATE_INT)){
 		echo $k . " must be an integer.<br>";
 	}
-	elseif ($params['min-multiplicand'] > $params['max-multiplicand'] || $params["min-multiplier"] > $params["max-multiplier"]){
+}
+	if ($params['min-multiplicand'] > $params['max-multiplicand'] || $params["min-multiplier"] > $params["max-multiplier"]){
 		if ($params['min-multiplicand'] > $params['max-multiplicand'])
 			echo "Miniumum multiplicand is larger than maximum.<br>";
 		if ($params["min-multiplier"] > $params["max-multiplier"])
 			echo "Miniumum multiplier is larger than maximum.<br>";
 	}
-}
+
 
 
 
